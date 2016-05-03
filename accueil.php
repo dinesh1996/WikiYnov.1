6f57b32f6ce6dae55acc9d1aa8fd854ab3acab9f
VALADIER Thomas <1.thomas.valadier@orange.fr>
2016-05-03 11:32:14 +0200

    formulaire 0.0

---
+++ accueil.php
@@ -0,0 +1,7 @@
+<html>
+<form action="" method="post">
+    <input name="login" id="login" type="text"><label for="login">Login</label>
+    <input name="password" id="password" type="password"><label for="password">Password</label>
+    <button type="submit" name="connexion">Se connecter</button>
+</form>
+</html>
