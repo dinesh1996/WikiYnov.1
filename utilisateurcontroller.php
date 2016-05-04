1b6fd58c3d115533309a73a0cbf3e3b6bf78f7d9
dinesh1996 <dinesh.salhotradass@ynov.com>
2016-05-05 00:07:49 +0200

    Delete utilisateurcontroller.php

--- utilisateurcontroller.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!isset($_POST['inscription'])) {
-    if (!empty ($_POST['login']) && !empty($_POST['password']) && !empty($_POST['email'])) {
-        $login = htmlspecialchars(addslashes(trim($_POST['login'])));
-        $password = htmlspecialchars(addslashes(trim($_POST['password'])));
-        $email = htmlspecialchars(addslashes(trim($_POST['email'])));
-        $user = new utilisateur ();
-    }
-}
