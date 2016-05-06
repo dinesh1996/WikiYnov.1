54d6510199b81dab26d61b1c3efa55930a417ea3
VALADIER Thomas <1.thomas.valadier@orange.fr>
2016-05-06 23:37:24 +0200

    formulaire 0.2

--- droitusers.php
+++ droitusers.php
@@ -3,9 +3,30 @@ session_start();
 require 'utilisateur.class.php';
 require 'db.class.php';
 
-if (($_SESSION['session']['rang']) == 'admin')
-    echo 'salut';
-else
+if (($_SESSION['session']['rang']) != 'admin')
     header('location:accueil.php');
+
+
+$db = new DB();
+$req = $db->requete("SELECT * FROM users");
+foreach ($req as $utilisateur):
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?= $utilisateur->login . " " . $utilisateur->password . " " . $utilisateur->rang; ?>
+<form><input name="id_user" value="<?= $utilisateur->id_user ?>" type="hidden">
+    <button type="submit">Créer admin</button>
+</form>
+<br>
+<?php endforeach; ?>
+
+</body>
+</html>
+
 
 
