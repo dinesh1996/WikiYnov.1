47fd988b2d7c336be9ff6affd460ffd6badda9db
seynabou01 <Seynabouba@MacBook-Air-de-Seynabou.local>
2016-05-08 14:18:42 +0200

    Amélioration1_PageArticle

--- pageArticleAA.php
+++ pageArticleAA.php
@@ -1,92 +1,113 @@
 <!DOCTYPE html>
 <html>
-     <head>
-          <meta charset="utf-8"/>
-          <title>PageArticles</title>
-          <link type="text/css" rel="stylesheet" href="pageArticle.css"/>
-         
-     </head>
+    <head>
+        <meta charset="utf-8"/>
+        <title>PageArticles</title>
+        <link type="text/css" rel="stylesheet" href="pageArticleAA.css"/>
+    </head>
     
-     <body>
+    <body>     
+        <?php include("enteteAA.php"); ?>
          
-         
-             
-             <?php include("enteteA.php"); ?>
-         
-<div style='width:1450px;height:500px;'>
-    
-         
-         
-         <div style='width:400px; height:300px; float:left; background-color:grey'>
-             
-            <font color="white"; size="6px">Ynov</font>
-             
-        </div>
-             
-        
-                        
-
-        
-         <div style='width:400px; height:300px;background-color:rgb(45, 200, 155); margin-left:400px'>
-             
-             <font color="white"; size="6px">Examens</font> 
-        </div>
-             
-                
-                  
-        
-        <div style='width:400px; height:300px; margin-left:800px;background-color:#F0E68C;margin-top:-300px'>
-             
-           
-          <font color="white"; size="6px">News</font>
+        <div class='articlesPanel'>
+            <div class='articlePanel200 blockLimeGreen blockDecalX1 blockDecalY1'>
+                <div class='articleTitle articlePanel200'>Ynov</div>   
+                <div class='articleContent articlePanel200'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div>   
+            </div>
             
-             
-      </div> 
-        
-    
-         
-          <div style='width:400px; height:300px;background-color:rgb(255, 150, 172);float:left'>
-              
-            <font color="white"; size="6px">Voyages</font> 
-         
-         </div>
-    
-    
-         
-         
-         <div style='width:400px; height:300px;background-color:rgb(200, 200, 200);margin-left:400px'>
-             
-            <font color="white"; size="6px">Offres</font> 
-             
-         </div>
-    
-          <div style='width:400px; height:300px;background-color:pink;margin-left:800px;margin-top:-300px'>
-             
-            <font color="white"; size="6px">djhcgbv</font> 
-             
-         </div>
-        
-    
-    </div>
-    
-<p></p>
-
-        
-
-         
-         
-            <?php include("pied_de_pageA.php"); ?>
-         
-      
-         
-      
-    
-    
-    
-    
-     </body>
-
-
-
-
+            <div class='articlePanel200 blockRoyalBlue blockDecalX1 blockDecalY2' >         
+                <div  class='articleTitle articlePanel200' >Examens</div> 
+                <div class='articleContent articlePanel200'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+            </div>
+            
+            <div class='articlePanel200 blockBlueViolet blockDecalX2 blockDecalY1' >
+                <div  class='articleTitle articlePanel200' >News</div>
+                <div class='articleContent articlePanel200'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+            </div>
+            
+            <div class='articlePanel200 blockRoyalBlue blockDecalX2 blockDecalY2'>
+                <div  class='articleTitle articlePanel200' >Voyages</div> 
+                <div class='articleContent articlePanel200'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+            </div>
+            
+            <div class='articlePanel400 blockBrown blockDecalX3 blockDecalY1'>
+                <div  class='articleTitle articlePanel400' >Offres</div> 
+                <div class='articleContent articlePanel400'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div>  
+            </div>
+            
+            <div class='articlePanel400 blockBlueViolet blockDecalX3 blockDecalY2' >
+                <div  class='articleTitle articlePanel400' >Messaging</div> 
+                <div class='articleContent articlePanel400'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+            </div>
+            
+            <div class='articlePanel400 blockTomato blockDecalX1 blockDecalY3' >
+                <div  class='articleTitle articlePanel400' >Mails</div> 
+                <div class='articleContent articlePanel400'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+            </div>
+            
+            <div class='articlePanel400 blockBack blockDecalX1 blockDecalY4' >
+                <div  class='articleTitle articlePanel400' >Microsoft</div> 
+                <div class='articleContent articlePanel400'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+            </div>
+            
+            <div class='articlePanel400 blockVioletRed blockDecalX3 blockDecalY3' >
+                <div  class='articleTitle articlePanel400' >Pamplemousse</div> 
+                <div class='articleContent articlePanel400'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+            </div>
+            
+            <div class='articlePanel400 blockVioletRed blockDecalX3 blockDecalY4' >
+                <div  class='articleTitle articlePanel400' >Boite de réception</div> 
+                <div class='articleContent articlePanel400'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+            </div>
+            
+            <div class='articlePanel400 blockBlueViolet blockDecalX6 blockDecalY1 ' >
+                <div  class='articleTitle articlePanel400' >Finance</div> 
+                <div class='articleContent articlePanel400'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+            </div>
+            
+            <div class='articlePanel200 blockRoyalBlue blockDecalX6 blockDecalY2' >
+                <div  class='articleTitle articlePanel200' >Alternance</div> 
+                <div class='articleContent articlePanel200'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+               
+            </div>
+            <div class='articlePanel200 blockTomato blockDecalX7 blockDecalY2' >
+                <div  class='articleTitle articlePanel200' >Bts</div> 
+                <div class='articleContent articlePanel200'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+            </div>
+            
+            <div class='articlePanel400 blockLimeGreen blockDecalX6 blockDecalY3' >
+                <div  class='articleTitle articlePanel400' >Annonces Stages</div> 
+                <div class='articleContent articlePanel400'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+            </div>
+            
+            <div class='articlePanel200 blockBrown blockDecalX6 blockDecalY4' >
+                <div  class='articleTitle articlePanel200' >Inscription</div> 
+                <div class='articleContent articlePanel200'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+            </div>
+            
+            <div class='articlePanel200 blockBack blockDecalX7 blockDecalY4' >
+                <div  class='articleTitle articlePanel200' >A propos du campus</div> 
+                <div class='articleContent articlePanel200'>At nunc si ad aliquem bene nummatum tumentemque
+                    Quare talis improborum consensio non modo. </div> 
+            </div>
+            
+        </div>
+        <p></p>
+        <?php include("pied_de_pageAA.php"); ?>
+    </body>
 </html>
